a3a56dc71b0a5f80c4dd0e9becf0cef0fadd5f5d
alex20santos <alexfosantos@hotmail.com>
2017-02-18 00:38:10 +0000

    Battles created and accepted, needs users for testing 22222 :)

--- server/MeteorMethods.js
+++ server/MeteorMethods.js
@@ -5,9 +5,24 @@ Meteor.methods({
           user: userId,
           createdAt: new Date()
     })
-  }
+  },
   
   'users.findId': function(username){
   	return Meteor.users.find({username: username});	
+  },
+
+  'battles.invite': function( opponent){
+    Battles.insert({
+      challenger: this._id,
+      opponent: opponent,
+      accepted: false,
+      createdAt: new Date()
+    })
+  },
+  'battles.start': function(battle){
+    Battle.update(battle, { $set: { accepted: true } });
+  },
+  'battle.delete': function(battle){
+    Battle.remove(battle);
   }
 });
