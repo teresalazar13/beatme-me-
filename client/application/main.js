550c68872ddcd3c15c68eec5d471c4c788804d72
teresalazar13 <teresa.sal13@gmail.com>
2017-02-18 15:49:23 +0000

    design bued cenas com joana maia

--- client/application/main.js
+++ client/application/main.js
@@ -3,16 +3,52 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
+Session.setDefault("toptext", "");
+Session.setDefault("bottomtext", "");
+Session.setDefault("urltext", "");
+
 Template.main.helpers({
   memes: function() {
     return Memes.find();
   },
+  /*
+
+  sourcePreview: function() {
+    url = "https://memegen.link/" + select + "/" + Session.get("toptext") + "/" + Session.get("bottomtext") + ".jpg";
+    console.log(url);
+    return url;
+  }*/
 });
 
 Template.main.events({
+  /*
+  'keyup #toptext': function (event) {
+    Session.set("toptext", event.target.value);
+  },
+
+  'keyup #urltext': function (event) {
+    Session.set("urltext", event.target.value);
+  },
+
+  'keyup #bottomtext': function (event) {
+    Session.set("bottomtext", event.target.value);
+  },
+
+  'click .meme-preview': function(event) {
+    var e = document.getElementById('meme-template');
+    var select = e.options[e.selectedIndex].value;
+    if (select == "default") {
+      url = "https://memegen.link/custom/" + Session.get("toptext") + "/" + Session.get("bottomtext")+ ".jpg?alt=" + Session.get("urltext");
+    }
+    else {
+      url = "https://memegen.link/" + select + "/" + Session.get("toptext") + "/" + Session.get("bottomtext") + ".jpg";
+    }
+    console.log(url);
+  },*/
+
   'click .meme-submit': function(event) {
     event.preventDefault();
-    var e = document.getElementById('memetemplate');
+    var e = document.getElementById('meme-template');
     var select = e.options[e.selectedIndex].value;
     var toptext = document.getElementById("toptext").value;
     var bottomtext = document.getElementById("bottomtext").value;
@@ -24,6 +60,7 @@ Template.main.events({
     else {
     	url = "https://memegen.link/" + select + "/" + toptext + "/" + bottomtext + ".jpg";
     }
+
   	Meteor.call('memes.insert', url);
   }
 });
