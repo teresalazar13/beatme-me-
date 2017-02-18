4e38fd87de222cda73ab9882b65fa7396bb19a7f
teresalazar13 <teresa.sal13@gmail.com>
2017-02-18 20:10:25 +0000

    changes header active fields acording to current path

--- client/application/header.js
+++ client/application/header.js
@@ -1,14 +1,38 @@
+Template.header.helpers({
+  username: function() {
+    return Meteor.user().username;
+  },
+
+  home_status: function() {
+    var url = decodeURIComponent(Router.current().location.get().path.split("/")[1]);
+    if (url === "")
+      return "active";
+  },
+
+  battles_status: function() {
+    var url = decodeURIComponent(Router.current().location.get().path.split("/")[1]);
+    if (url == "battles")
+      return "active";
+  },
+
+  leaderboard_status: function() {
+    var url = decodeURIComponent(Router.current().location.get().path.split("/")[1]);
+    if (url == "leaderboard")
+      return "active";
+  },
+
+  profile_status: function() {
+    var url = decodeURIComponent(Router.current().location.get().path.split("/")[1]);
+    if (url == "profile")
+      return "active";
+  }
+});
+
 Template.header.events({
   'click #logout': function(event) {
     event.preventDefault();
     Meteor.logout();
     Router.go('/');
     document.location.reload(true);
-  }
-});
-
-Template.header.helpers({
-  username: function() {
-    return Meteor.user().username;
-  }
+  },
 });
