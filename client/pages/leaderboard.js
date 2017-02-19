a234656fde440cecdede312c3e5fec90114680fb
alex20santos <alexfosantos@hotmail.com>
2017-02-19 12:44:51 +0000

    leaderboard :nerd_face:

--- client/pages/leaderboard.js
+++ client/pages/leaderboard.js
@@ -1,5 +1,16 @@
 Template.leaderboard.helpers({
-	leaderboard: function(){
-		return Meteor.users.find({},{sort: users.victories}).limit(10);	
-	}
+	users: function(){
+		return Meteor.users.find({},{$sort: {victories: 1}});
+	},
+	username: function(){
+		return this.username;
+	},
+	victories: function(){
+		 return this.victories;
+ },
+ position: function(){
+	 var aux = Meteor.users.find({},{$sort: {victories: 1}});
+	 var index = aux.indexOf(this);
+	 return index +1;
+ }
 });
