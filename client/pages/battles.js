7447a2370496a25c492451d47a922f00377d4177
teresalazar13 <teresa.sal13@gmail.com>
2017-02-19 03:14:30 +0000

    design com joana maia

--- client/pages/battles.js
+++ client/pages/battles.js
@@ -6,7 +6,24 @@ import './battles.html';
 Template.battles.helpers({
   battlesList: function() {
     return Battles.find();
-  }
+  },
+
+  challengerUsername: function() {
+    return Meteor.users.findOne({"_id": this.challenger}).username;
+  },
+
+  opponentUsername: function() {
+    return Meteor.users.findOne({"_id": this.opponent}).username;
+  },
+
+  roundNumber: function() {
+    return Meteor.battles.find({"_id": this._id}).length;
+  },
+
+  category: function() {
+    var catArray = Meteor.battles.find({"_id": this._id}).categories;
+    return catArray[catArray.length-1];
+  },
 });
 
 Template.battles.events({
